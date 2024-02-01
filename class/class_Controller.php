c1a77da1af981967fc8ca8ff2197aa03c1e0c02c
samuel12ae <samuel12ae@gmail.com>
2024-02-01 16:22:17 +0100

    commit 2

--- class/class_Controller.php
+++ class/class_Controller.php
@@ -29,13 +29,52 @@ class Controller {
         if($id) {
             // Fehlermeldung User existiert schon
             $this->info('Bitte anderen Namen wählen', 'red');
-        } else {
-            // User kann eingetragen werden
-            $hash = hash('sha512', $this->r['pass']); // sicherer Abspeichern
-            Model::setUserIntoDB($this->r['name'], $this->r['e_mail'], $hash);
-            $this->info('Anmeldung erfolgreich', 'green');
+            $this->v->setLayout('anmelden');
+            return false;
         }
+
+        // Passwort Komlexitätstest
+        if($this->getKomplex($this->r['pass']) === false) {
+            $this->info('Komplexitätsanforderungen nicht erfüllt', 'red');
+            $this->v->setLayout('anmelden');
+            return false; // weiteres Script wird in dieser Methode nicht ausgeführt
+        }
+
+        $id = Model::getIdFromEmail($this->r['e_mail']);
+        if($id) {
+            // Fehlermeldung E-mail existiert schon
+            $this->info('E-Mail existiert bereits', 'red');
+            $this->v->setLayout('anmelden');
+            return false;
+        }
+
+        //Valide E-mail
+        if(!filter_var($this->r['e_mail'], FILTER_VALIDATE_EMAIL)) {
+            $this->info('E-Mail nicht korrekt', 'red');
+            $this->v->setLayout('anmelden');
+            return false;
+        }
+
+        // Erfolgreich anmelden
+        $hash = hash('sha512', $this->r['pass']); // sicherer Abspeichern
+        Model::setUserIntoDB($this->r['name'], $this->r['e_mail'], $hash);
+        $this->info('Anmeldung erfolgreich', 'green');
         $this->v->setLayout('anmelden'); //welche Seite soll gezeigt werden
+        return true;
+    }
+
+    private function getKomplex($pass) {
+        // assiziatives Array ['L':true, 'G':true, 'K':true, 'S':true, 'I':true] = Länge 5
+        $komplex = [];
+        if(strlen($pass) >= 10) $komplex['L'] = true;
+        for($i = 0; $i < strlen($pass); $i++) {
+            if(ctype_upper($pass[$i])) $komplex['G'] = true;
+            if(ctype_lower($pass[$i])) $komplex['K'] = true;
+            if(ctype_punct($pass[$i])) $komplex['S'] = true;
+            if(ctype_digit($pass[$i])) $komplex['I'] = true;
+        }
+        if(count($komplex) == 5) return true; // kein weiterer Code
+        return false;
     }
 
     private function info($text, $color) {
--- class/class_Model.php
+++ class/class_Model.php
@@ -8,6 +8,13 @@ class Model {
         return SERVICE::getOneFromDB($mask);
     }
 
+    public static function getIdFromEmail($e_mail) {
+        // SQL Iniection sicher
+        $mask = SERVICE::setPrepare('SELECT id FROM tb_user WHERE e_mail = ?'); // in Zwieschenspeicher legen
+        $mask->bindValue(1, $e_mail, PDO::PARAM_STR);
+        return SERVICE::getOneFromDB($mask);
+    }
+    
     public static function setUserIntoDB($name, $e_mail, $pass) {
         // status = 4 = User? (nicht verifiziert)
         $mask = SERVICE::setPrepare('INSERT INTO tb_user (name, pass, e_mail, id_status)
